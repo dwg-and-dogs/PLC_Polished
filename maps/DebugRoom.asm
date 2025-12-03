f2d0cd7ca41c5a9f54f78574475fb7aa2475b30c
David-Gardner <dwg@serinuslabs.com>
2025-12-03 15:08:51 -0800

    moved HOF from DR to HHC, need to fix movement in HHC

--- maps/DebugRoom.asm
+++ maps/DebugRoom.asm
@@ -29,54 +29,7 @@ DebugRoom_MapScriptHeader:
 	object_const_def
 	const DEBUG_DWG
 
-LoadSelectedMonData:
-    ; --- 1. FIND ADDRESS OF CHOSEN MON'S STRUCT ---
-    ld a, [wCurPartyMon]        ; Get the slot number (0-5)
-    ld hl, wPartyMons           ; Start of party data
-    ld bc, PARTYMON_STRUCT_LENGTH ; Size of one mon entry
-    rst AddNTimes               ; HL = wPartyMons + (Slot * StructSize)
-    
-    ; HL is now pointing to the start of the specific mon's data
-    ; Save this location because we need to peek at it twice (Species & Level)
-    push hl                     
-    
-    ; --- 2. GET SPECIES NAME -> wStringBuffer1 ---
-    ld a, [hl]                  ; First byte of struct is ALWAYS Species ID
-    ld [wNamedObjectIndex], a   ; Set for lookup
-    call GetPokemonName         ; Writes name to wStringBuffer1
-    
-    pop hl                      ; Restore pointer to the struct
-    push hl                     ; Save it again for the Level step
-    
-    ; --- 3. GET LEVEL -> wStringBuffer3 ---
-    ld bc, MON_LEVEL            ; The distance from start of struct to Level byte
-    add hl, bc                  ; HL now points directly at the Level byte
-    
-    ld d, h                     ; Move HL to DE (PrintNum needs Source in DE)
-    ld e, l                     
-    
-    ld hl, wStringBuffer3       ; Destination Buffer
-    lb bc, 1, 3                 ; 1 Byte, 3 Digits
-    call PrintNum               ; Convert level number to text
-    ld [hl], "@"                ; Add terminator
 
-    pop hl                      ; Restore pointer (just to clean up stack)
-
-    ; --- 4. GET NICKNAME -> wStringBuffer2 ---
-    ld a, [wCurPartyMon]        ; Reload slot number
-    ld hl, wPartyMonNicknames   ; Start of nickname list
-    ld bc, MON_NAME_LENGTH      ; Length of one name
-    rst AddNTimes               ; HL = wPartyMonNicknames + (Slot * 11)
-    
-    ld d, h                     ; Move Source to DE (Wait, CopyBytes is HL->DE usually?)
-    ld e, l                     ; Let's check standard CopyBytes usage:
-                                ; It is usually HL (Source) -> DE (Dest).
-                                ; So keep Source in HL.
-    
-    ld de, wStringBuffer2       ; Destination
-    ld bc, MON_NAME_LENGTH      ; Length to copy
-    rst CopyBytes               ; Copy the name
-    ret
 
 DebugInteraction: 
 	applyonemovement PLAYER, hide_object
@@ -97,13 +50,16 @@ DebugInteraction:
 	special FadeOutPalettes
 
 	loadmem wCurForm, -1 ; force
-	loadmem wCurPartyMon, 1    ; Select first party slot
+	loadmem wCurPartyMon, 0    ; Select first party slot
 	readmem wPartyMon1Species  ; or wCurPartySpecies... seems to work either way 
 
 	callasm LoadSelectedMonData ; Calculates everything for Slot 2 automatically    
 
     opentext
-    pokepic2 1
+	loadmem wCurForm, -1
+	loadmem wCurPartyMon, 1    ; Select first party slot
+	readmem wPartyMon2Species  ; or wCurPartySpecies
+	pokepic2 0	
     writethistext
         text_ram wStringBuffer1 ; "PIDGEY"
         text " Lv."             ; " Lv."
--- maps/HollysHoltCredit.asm
+++ maps/HollysHoltCredit.asm
@@ -21,6 +21,54 @@ HollysHoltCredit_MapScriptHeader:
 	object_event 17, 37, SPRITE_BREEDER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1 
 	tmhmball_event 24, 33, TM_X_SCISSOR, EVENT_GOT_TM_X_SCISSOR
 
+LoadSelectedMonData::
+    ; --- 1. FIND ADDRESS OF CHOSEN MON'S STRUCT ---
+    ld a, [wCurPartyMon]        ; Get the slot number (0-5)
+    ld hl, wPartyMons           ; Start of party data
+    ld bc, PARTYMON_STRUCT_LENGTH ; Size of one mon entry
+    rst AddNTimes               ; HL = wPartyMons + (Slot * StructSize)
+    
+    ; HL is now pointing to the start of the specific mon's data
+    ; Save this location because we need to peek at it twice (Species & Level)
+    push hl                     
+    
+    ; --- 2. GET SPECIES NAME -> wStringBuffer1 ---
+    ld a, [hl]                  ; First byte of struct is ALWAYS Species ID
+    ld [wNamedObjectIndex], a   ; Set for lookup
+    call GetPokemonName         ; Writes name to wStringBuffer1
+    
+    pop hl                      ; Restore pointer to the struct
+    push hl                     ; Save it again for the Level step
+    
+    ; --- 3. GET LEVEL -> wStringBuffer3 ---
+    ld bc, MON_LEVEL            ; The distance from start of struct to Level byte
+    add hl, bc                  ; HL now points directly at the Level byte
+    
+    ld d, h                     ; Move HL to DE (PrintNum needs Source in DE)
+    ld e, l                     
+    
+    ld hl, wStringBuffer3       ; Destination Buffer
+    lb bc, 1, 3                 ; 1 Byte, 3 Digits
+    call PrintNum               ; Convert level number to text
+    ld [hl], "@"                ; Add terminator
+
+    pop hl                      ; Restore pointer (just to clean up stack)
+
+    ; --- 4. GET NICKNAME -> wStringBuffer2 ---
+    ld a, [wCurPartyMon]        ; Reload slot number
+    ld hl, wPartyMonNicknames   ; Start of nickname list
+    ld bc, MON_NAME_LENGTH      ; Length of one name
+    rst AddNTimes               ; HL = wPartyMonNicknames + (Slot * 11)
+    
+    ld d, h                     ; Move Source to DE (Wait, CopyBytes is HL->DE usually?)
+    ld e, l                     ; Let's check standard CopyBytes usage:
+                                ; It is usually HL (Source) -> DE (Dest).
+                                ; So keep Source in HL.
+    
+    ld de, wStringBuffer2       ; Destination
+    ld bc, MON_NAME_LENGTH      ; Length to copy
+    rst CopyBytes               ; Copy the name
+    ret
 
 HollyHoltCreditScene: 
 	;MOVING CAMERA
@@ -30,25 +78,41 @@ HollyHoltCreditScene:
 	pause 120
 	applymovement PLAYER, HH_Credit_Move1
 	pause 120
-	applymovement PLAYER, HH_Credit_Move2
+	applymovement PLAYER, HH_Credit_Move2 ; todo, revise the path that is taken so that it ends up on the kimono house 
 	pause 120
 	applymovement PLAYER, HH_Credit_Move3
 	pause 120
 	; hall of fame section 
-	refreshscreen
-	loadmem wCurForm, -1
+	special FadeOutPalettes
+
+	loadmem wCurForm, -1 ; force
 	loadmem wCurPartyMon, 0    ; Select first party slot
+	readmem wPartyMon1Species  ; or wCurPartySpecies... seems to work either way 
+
+	callasm LoadSelectedMonData ; Calculates everything for Slot 2 automatically    
+
+    opentext
+	loadmem wCurForm, -1
+	loadmem wCurPartyMon, 1    ; Select first party slot
 	readmem wPartyMon1Species  ; or wCurPartySpecies
-	pokepic 0	
-	pause 120
+	pokepic2 0	
+    writethistext
+        text_ram wStringBuffer1 ; "PIDGEY"
+        text " Lv."             ; " Lv."
+        text_ram wStringBuffer3 ; "  5"
+        line " "                ; New line
+        text_ram wStringBuffer2 ; "BIRDY"
+        done
+    pause 120
 	waitbutton
-	closepokepic
-	pause 60
+	closetext
+
+
+
 	setevent EVENT_BURNED_TOWER_B1F_BEASTS_1
 	clearevent EVENT_BURNED_TOWER_B1F_BEASTS_2
 	setevent EVENT_STADIUM_GROUNDS_FIRST_FLOOD
 	setevent EVENT_STADIUM_GROUNDS_SECOND_FLOOD
-	special FadeOutPalettes
 	warp STADIUM_GROUNDS_CREDIT, 8, 35
 	end
 
