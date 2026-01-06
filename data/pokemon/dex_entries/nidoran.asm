319b7fa5d7c5d3d5ce9fbd40165bede42bff6ad8
David-Gardner <dwg@serinuslabs.com>
2026-01-06 15:18:03 -0800

    add nidoran dex entry

---
+++ data/pokemon/dex_entries/nidoran.asm
@@ -0,0 +1,9 @@
+	db "Poison Pin@" ; species name
+	dw 108, 176 ; height, width
+
+	db   "Whether it evolves"
+	next "to Nidorino or to"
+	next "Nidorina depends"
+	page "on whether it"
+	next "prefers attack"
+	next "or defense.@"
