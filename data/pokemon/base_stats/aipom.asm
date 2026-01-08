6ad097b3ccec9a1a6df60b34acc38e621fedd975
David-Gardner <dwg@serinuslabs.com>
2026-01-08 07:19:58 -0800

    many pokemon icons and front back

---
+++ data/pokemon/base_stats/aipom.asm
@@ -0,0 +1,20 @@
+	db  75, 100,  66, 115,  60,  66 ; 510 BST
+	;   hp  atk  def  spd  sat  sdf
+
+	db NORMAL, NORMAL ; type
+	db 45 ; catch rate
+	db 94 ; base exp
+	db NO_ITEM ; item 1
+	db NO_ITEM ; item 2
+	dn GENDER_F50, 3 ; gender ratio, step cycles to hatch
+	INCBIN "gfx/pokemon/aipom/front.dimensions"
+	abilities_for AIPOM, RUN_AWAY, PICKUP, MOODY
+	db GROWTH_FAST ; growth rate
+	dn EGG_MONSTER, EGG_GROUND ; egg groups
+
+	ev_yield   0,   0,   0,   1,   0,   0
+	;         hp  atk  def  spd  sat  sdf
+
+	; tm/hm learnset
+	tmhm DYNAMICPUNCH, CURSE, TOXIC, HAIL, HIDDEN_POWER, SUNNY_DAY, LIGHT_SCREEN, PROTECT, RAIN_DANCE, SAFEGUARD, BULLDOZE, THUNDERBOLT, THUNDER, RETURN, DIG, ROCK_SMASH, DOUBLE_TEAM, REFLECT, SWIFT, SUBSTITUTE, FACADE, REST, ATTRACT, THIEF, FOCUS_BLAST, DRAIN_PUNCH, ACROBATICS, WATER_PULSE, SHADOW_CLAW, POISON_JAB, U_TURN, THUNDER_WAVE, CUT, STRENGTH, BATON_PASS, BODY_SLAM, CHARM, COUNTER, DEFENSE_CURL, DOUBLE_EDGE, ENDURE, FIRE_PUNCH, HEADBUTT, KNOCK_OFF, SEISMIC_TOSS, SLEEP_TALK, SUCKER_PUNCH, SWAGGER, THUNDERPUNCH, ZEN_HEADBUTT
+	; end
