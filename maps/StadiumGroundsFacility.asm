367c113cfdae552fb5d8c12b0b6c488481adee34
David-Gardner <dwg@serinuslabs.com>
2026-06-02 08:12:31 -0700

    npcs for prep area

--- maps/StadiumGroundsFacility.asm
+++ maps/StadiumGroundsFacility.asm
@@ -7,7 +7,7 @@ StadiumGroundsFacility_MapScriptHeader:
 	; spawn point here unless you have not yet beaten the game then it sends you to azalea 
 	
 	def_callbacks
-	callback MAPCALLBACK_OBJECTS, StadiumGroundsFacilityNPCs ; c.f. the hidden groto 
+;	callback MAPCALLBACK_OBJECTS, StadiumGroundsFacilityNPCs ; c.f. the hidden groto 
 	
 	def_warp_events
 	; you're warped in and out of these 
@@ -45,23 +45,23 @@ StadiumGroundsFacility_MapScriptHeader:
 	object_const_def
 
 
-StadiumGroundsFacilityScript:
+;StadiumGroundsFacilityScript:
 	; random 1-5, for now we just do these in order
 	; set the spawn to gauldenrod 
-	opentext
-	writethistext
-		text "<PLAYER>!"
-		line "Prove youself as"
-		cont "a master of both"
-		cont "catching and"
-		cont "battling!"
-		done
-	waitbutton
-	closetext
+;	opentext
+;	writethistext
+;		text "<PLAYER>!"
+;		line "Prove youself as"
+;		cont "a master of both"
+;		cont "catching and"
+;		cont "battling!"
+;		done
+;	waitbutton
+;	closetext
 	
-	loadvar VAR_BATTLETYPE, BATTLETYPE_TRAP
+;	loadvar VAR_BATTLETYPE, BATTLETYPE_TRAP
 	; random 1- 5
-	loadwildmon SCYTHER, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS -- SCYTHER LEFTIES
+;	loadwildmon SCYTHER, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS -- SCYTHER LEFTIES
 ;	loadwildmon KLEAVOR, 100 ; CHECK MOVES AT LV 100  HARD STONE 
 ;	loadwildmon MINSIR, 100 ; CHECK MOVES AT LV 100  CHOICE SCARF 
 ;	loadwildmon YANMEGA, 100 ; CHECK MOVES AT LV 100 CHOICE SPECS 
@@ -70,8 +70,8 @@ StadiumGroundsFacilityScript:
 ; HERACROSS 
 ; FORRETRESS 
 ; AMAGCARGO 
-	startbattle
-	ifequal $1, .Continue
+;	startbattle
+;	ifequal $1, .Continue
 	
 	; heal the PLAYER
 	; allow them to reorganize the party and items 
@@ -79,24 +79,24 @@ StadiumGroundsFacilityScript:
 	
 	; now start the trainers, up to three 
 
-	winlosstext SGF_Tier1_WinText, SGF_Tier1_LoseText
+;	winlosstext SGF_Tier1_WinText, SGF_Tier1_LoseText
 	; START 
 ; random 1 - 3
-	loadtrainer BUG_MANIAC, BUG_MANIAC_SGF_1
-	startbattle
-	ifequal $1, .Continue2
-.Continue2:
-	reloadmapafterbattle
+;	loadtrainer BUG_MANIAC, BUG_MANIAC_SGF_1
+;	startbattle
+;	ifequal $1, .Continue2
+;.Continue2:
+;	reloadmapafterbattle
 ; random 4 - 6
-	winlosstext SGF_Tier1_WinText, SGF_Tier1_LoseText
-	loadtrainer BUG_MANIAC, BUG_MANIAC_SGF_4
-	startbattle
-	ifequal $1, .Continue2
+;	winlosstext SGF_Tier1_WinText, SGF_Tier1_LoseText
+;	loadtrainer BUG_MANIAC, BUG_MANIAC_SGF_4
+;	startbattle
+;	ifequal $1, .Continue2
 ; random 7 - 9
-	winlosstext SGF_Tier1_WinText, SGF_Tier1_LoseText
-	loadtrainer BUG_MANIAC, BUG_MANIAC_SGF_7
-	startbattle
-	ifequal $1, .Continue2
+;	winlosstext SGF_Tier1_WinText, SGF_Tier1_LoseText
+;	loadtrainer BUG_MANIAC, BUG_MANIAC_SGF_7
+;	startbattle
+;	ifequal $1, .Continue2
 
 
 
@@ -104,7 +104,7 @@ StadiumGroundsFacilityScript:
 
 
 ; SANDRAS MONS 
-	loadwildmon CLEFABLE, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS
+;	loadwildmon CLEFABLE, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS
 ;	loadwildmon RAPIDASH GALARIAN, 100 ; CHECK MOVES AT LV 100 
 ;	loadwildmon GALARIAN WEEZING, 100 ; CHECK MOVES AT LV 100  CHOICE SCARF 
 ;	loadwildmon SNEASLER, 100 ; CHECK MOVES AT LV 100 CHOICE SPECS 
@@ -121,7 +121,7 @@ StadiumGroundsFacilityScript:
 
 
 ; SYBIL MONS 
-	loadwildmon MISMAGIUS, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS
+;	loadwildmon MISMAGIUS, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS
 ;	loadwildmon GENGAR, 100 ; CHECK MOVES AT LV 100 
 ;	loadwildmon WYRDEER, 100 ; CHECK MOVES AT LV 100  CHOICE SCARF 
 ;	loadwildmon CROBAT, 100 ; CHECK MOVES AT LV 100 CHOICE SPECS 
@@ -136,7 +136,7 @@ StadiumGroundsFacilityScript:
 ; AXATU 
 
 ; REMY  MONS 
-	loadwildmon LANTURN, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS
+;	loadwildmon LANTURN, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS
 ;	loadwildmon QUGASIRE , 100 ; CHECK MOVES AT LV 100 
 ;	loadwildmon GDOS, 100 ; CHECK MOVES AT LV 100  CHOICE SCARF 
 ;	loadwildmon JUMPLUFF, 100 ; CHECK MOVES AT LV 100 CHOICE SPECS 
@@ -148,7 +148,7 @@ StadiumGroundsFacilityScript:
 
 
 ; AMOS   MONS 
-	loadwildmon HGOODRA, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS
+;	loadwildmon HGOODRA, 100 ; CHECK MOVES AT LV 100  TODO AND ALSO ADD CUSTOM ITEMS
 ;	loadwildmon ABOMOASNOW  , 100 ; CHECK MOVES AT LV 100 
 ;	loadwildmon GALLADE, 100 ; CHECK MOVES AT LV 100  CHOICE SCARF 
 ;	loadwildmon FERALIGAR, 100 ; CHECK MOVES AT LV 100 CHOICE SPECS 
--- maps/StadiumGroundsFacilityPrep.asm
+++ maps/StadiumGroundsFacilityPrep.asm
@@ -19,22 +19,40 @@ StadiumGroundsFacilityPrep_MapScriptHeader:
 
 
 
-	def_object_events ; depending on day of the week 
-	; clerk
-	; rematch ursula, hollis
-	; beth, sandra
-	; sybil
-	; nomadf mom, barbeau 
-	; petra
-	; tyson 
-	; amos
-	; kimono girl from celebi 
-	; kurt's natu 
-	
+	def_object_events ; depending on time of day? 
+	object_event 15, 20, SPRITE_MON_ICON, SPRITEMOVEDATA_POKEMON, 0, NATU, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, FacilityNatuScript, -1
+	object_event 14, 10, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, StadiumGroundsFaciltyPrepClerkScript, -1 ; todo
+	object_event 11, 14, SPRITE_TAMMY, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FacilityUrsulaScript, -1 ; todo 
+	object_event 29, 20, SPRITE_HOLLIS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FacilitySilasScript, -1 ; todo 
+	object_event 12, 14, SPRITE_ACE_TRAINER_F, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FacilityBethScript, -1 ; todo 
+	object_event 26, 19, SPRITE_SANDRA, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FacilitySandraScript, -1 ; todo 
+	object_event 28, 18, SPRITE_SAMSARA, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FacilitySybilScript, -1 ; todo 
+	object_event 16, 12, SPRITE_BRIGADER, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FacilityTysonScript, -1 ; todo 
+	object_event 16, 18, SPRITE_NOMAD_F, 	SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, FacilityNomadFScript, -1	; todo only appears if you return the nomad 
+	object_event 25, 17, SPRITE_BARBEAU, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_PURPLE, OBJECTTYPE_SCRIPT, 0, FacilityBarbeauScript, -1  ; todo 
+	object_event 12, 20, SPRITE_KANNA, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, FacilityPetraScript, -1  ; todo 
+	object_event 12, 18, SPRITE_AMOS, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FacilityAmosScript, -1 ; todo 
+	object_event 12, 18, SPRITE_AMOS, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FacilityAmosScript, -1 ; todo 
+	object_event  3, 19, SPRITE_KIMONO_GIRL, SPRITEMOVEDATA_WANDER, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, FacilityPiperScript, -1 ; todo 
+
+
 	object_const_def
 
 
+FacilityNatuScript:
+	opentext
+	writetext NatuFacilityText
+	promptbutton
+	special PokemonCenterPC
+	endtext
+	end
 
+NatuFacilityText:
+	text "It's Kurt's Natu!"
+	line "It can teleport"
+	para "to the present to"
+	line "manage the party."
+	done	
 
 StadiumGroundsFaciltyPrepClerkScript:
 	faceplayer
@@ -108,6 +126,11 @@ FacilityPrep_ExplainFullText:
 	line "and then a final"
 	para "boss for the"
 	line "round."
+	
+	para "You may enter at"
+	line "whatever level"
+	para "you like, but I"
+	line "suggest Lv. 100."
 	done
 
 FacilityPrep_AreYouReadyText:
